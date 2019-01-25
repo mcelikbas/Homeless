7c94a96771e2e9c453e0ca64e0bc0cadcbec5073
mcelikbas <mikail.celikbas@gmail.com>
2019-01-26 00:29:43 +0100

    item script

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item {
+
+    private string itemName;
+    private string description;
+
+    public Item (string itemName, string description)
+    {
+        this.ItemName = itemName;
+        this.Description = description;
+    }
+
+    public string ItemName
+    {
+        get
+        {
+            return itemName;
+        }
+
+        set
+        {
+            itemName = value;
+        }
+    }
+
+    public string Description
+    {
+        get
+        {
+            return description;
+        }
+
+        set
+        {
+            description = value;
+        }
+    }
+}
