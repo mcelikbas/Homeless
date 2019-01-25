2c8e22d94752d4fe8c088fbb4f874c3d9481c6bb
localbea <localbea@D32-19-S.bellecour.int>
2019-01-26 00:27:07 +0100

    camera follow commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+
+    //public float smoothSpeed = 1f;
+    private Vector3 offset = new Vector3(0f,0f,-1f);
+
+
+
+	void LateUpdate () {
+
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = desiredPosition;
+
+        
+        //Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        //transform.position = smoothedPosition;
+       // transform.LookAt(target);
+
+	}
+}
