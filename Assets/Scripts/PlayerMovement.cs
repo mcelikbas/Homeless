6682188b0a67c76288c219657caeea3e064a6afc
localbea <localbea@D32-18-S.bellecour.int>
2019-01-25 22:07:29 -0100

    player movement + mouse look

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,15 +5,35 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     private float speed = 5.0f;
+    private float rotateSpeed ;
     private float inputX;
     private float inputY;
 
 
-	void Update () {
 
+
+	void Update ()
+    {
+        move();
+        //mouseLook();
+    }
+
+
+    private void move ()
+    {
         inputX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         inputY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 
         transform.Translate(inputX, inputY, 0);
+    }
+
+    private void mouseLook ()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        Vector2 direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+
+        transform.up = direction;
     }
 }
