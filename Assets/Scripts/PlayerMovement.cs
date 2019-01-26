2ab29e5dc62dea23dde2558b04238756c0b486a7
mcelikbas <mikail.celikbas@gmail.com>
2019-01-26 22:19:18 +0100

    movement updated with facing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,21 @@ public class PlayerMovement : MonoBehaviour {
     private float inputX;
     private float inputY;
 
+    private Vector2 lastInput = new Vector2(0,1);
 
+    public Sprite spriteTop;
+    public Sprite spriteRight;
+    private SpriteRenderer spriteRenderer;
 
+    private void Start ()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
-	void Update ()
+    void Update ()
     {
         move();
+        updateSprite();
         //mouseLook();
     }
 
@@ -24,8 +33,37 @@ public class PlayerMovement : MonoBehaviour {
         inputX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         inputY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 
+        lastInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
         transform.Translate(inputX, inputY, 0);
     }
+
+    private void updateSprite ()
+    {
+        if(lastInput.y == 1)
+        {
+            spriteRenderer.sprite = spriteTop;
+            spriteRenderer.flipY = false;
+        }
+        else if (lastInput.y == -1)
+        {
+            spriteRenderer.sprite = spriteTop;
+            spriteRenderer.flipY = true;
+        }
+        
+        if (lastInput.x == 1)
+        {
+            spriteRenderer.sprite = spriteRight;
+            spriteRenderer.flipX = false;
+        }
+        else if (lastInput.x == -1)
+        {
+            spriteRenderer.sprite = spriteRight;
+            spriteRenderer.flipX = true;
+        }
+
+    }
+
 
     private void mouseLook ()
     {
@@ -36,4 +74,8 @@ public class PlayerMovement : MonoBehaviour {
 
         transform.up = direction;
     }
+
+
+
+
 }
