7262b3168a4226e551d7da35917241bf4352dad4
mcelikbas <mikail.celikbas@gmail.com>
2019-01-27 00:22:48 +0100

    pick up item and add to inventory

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour {
 
@@ -15,8 +16,21 @@ public class PlayerMovement : MonoBehaviour {
     public Sprite spriteRight;
     private SpriteRenderer spriteRenderer;
 
+
+    private GameObject INV_MANAGER;
+    private bool inventoryIsHidden = true;
+    private ItemDatabase itemDatabase;
+    private Inventory inventory;
+
+
     private void Start ()
     {
+        INV_MANAGER = GameObject.FindGameObjectWithTag("INV_MANAGER");
+        INV_MANAGER.transform.GetChild(0).gameObject.SetActive(false);
+
+        itemDatabase = GameObject.Find("INV_CRAFT").GetComponent<ItemDatabase>();
+        inventory = GameObject.Find("INV_CRAFT").GetComponent<Inventory>();
+
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
@@ -25,6 +39,19 @@ public class PlayerMovement : MonoBehaviour {
         move();
         updateSprite();
         //mouseLook();
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (inventoryIsHidden)
+            {
+                INV_MANAGER.transform.GetChild(0).gameObject.SetActive(true);
+            }
+            else
+            {
+                INV_MANAGER.transform.GetChild(0).gameObject.SetActive(false);
+            }
+            inventoryIsHidden = !inventoryIsHidden;
+        }
     }
 
 
@@ -54,15 +81,34 @@ public class PlayerMovement : MonoBehaviour {
         if (lastInput.x == 1)
         {
             spriteRenderer.sprite = spriteRight;
+            spriteRenderer.flipY = false;
             spriteRenderer.flipX = false;
         }
         else if (lastInput.x == -1)
         {
             spriteRenderer.sprite = spriteRight;
+            spriteRenderer.flipY = false;
             spriteRenderer.flipX = true;
         }
 
     }
+
+    private void pickUpItem (int itemId, int qty)
+    {
+        inventory.addItem(itemDatabase.getItemById(itemId), qty);
+    }
+
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.CompareTag("Item"))
+        {
+            ItemToPick itemToPick = other.GetComponent<ItemToPick>();
+
+            Debug.Log("player hit" + other.name);
+            pickUpItem(itemToPick.id, itemToPick.qty);
+        }
+    }
+
 
 
     private void mouseLook ()
